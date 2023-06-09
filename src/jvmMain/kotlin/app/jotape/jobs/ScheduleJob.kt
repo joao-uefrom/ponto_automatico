267c3562e722d64cc2ce1f4b6579f016e51c5290
João Fernandes <joao.fernandesds26@gmail.com>
2023-06-09 11:34:39 -0300

    added job for PunchTheClock in background.

--- src/jvmMain/kotlin/app/jotape/jobs/ScheduleJob.kt
+++ src/jvmMain/kotlin/app/jotape/jobs/ScheduleJob.kt
@@ -1,12 +1,21 @@
 package app.jotape.jobs
 
+import app.jotape.models.Configuration
+import app.jotape.services.HttpService
+import app.jotape.services.SchedulerService
 import org.quartz.Job
 import org.quartz.JobExecutionContext
 
 class ScheduleJob : Job {
 
     override fun execute(context: JobExecutionContext) {
-        println("ScheduleJob.execute()")
+        Configuration.user()?.let {
+            HttpService.login(it)
+            HttpService.punchTheClock()
+            HttpService.quit()
+        }
+
+        SchedulerService.toNext()
     }
 
 }
